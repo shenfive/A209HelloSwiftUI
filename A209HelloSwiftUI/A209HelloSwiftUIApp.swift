c7e0412bd91072607d307997d69835de4e3a811f
shenfive@gmail.com <Danny Shen>
2022-11-05 17:13:16 +0800

    Initial Commit

---
+++ A209HelloSwiftUI/A209HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  A209HelloSwiftUIApp.swift
+//  A209HelloSwiftUI
+//
+//  Created by 申潤五 on 2022/11/5.
+//
+
+import SwiftUI
+
+@main
+struct A209HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
