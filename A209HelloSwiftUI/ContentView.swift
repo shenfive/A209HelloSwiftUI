a38d4169b39cf3e587ca6bebb09f8272d30900bd
shenfive@gmail.com <Danny Shen>
2022-11-12 11:16:19 +0800

    add remove

--- A209HelloSwiftUI/ContentView.swift
+++ A209HelloSwiftUI/ContentView.swift
@@ -8,16 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    
+    
+    @State var emojiCount = 5
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            Spacer()
+            HStack{
+                remove
+                Spacer()
+                add
+            }
         }
-        .padding()
     }
+    
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+            print(emojiCount)
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View{
+        Button{
+            if emojiCount <= 20{
+                emojiCount += 1
+            }
+            print(emojiCount)
+        } label: {
+            Image(systemName: "plus.circle")
+        }
+        
+    }
+    
+    
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
